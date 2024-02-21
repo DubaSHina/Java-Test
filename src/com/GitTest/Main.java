7f40f55ad8031250ba42002cb2e992bc491e780b
DubaSHina <Volkoniker134@yandex.ru>
2024-02-22 02:08:12 +0300

    Refactoring

--- src/com/GitTest/Main.java
+++ src/com/GitTest/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        System.out.println("Напишите своё имя : ");
+        System.out.println("What is your name : ");
         String name = console.nextLine();
         System.out.println("Рады вас приветствовать: " + name + "!");
 
