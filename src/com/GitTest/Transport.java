ee77e377d7290a5d85c8133df6bb674af458dd57
DubaSHina <Volkoniker134@yandex.ru>
2024-02-22 03:58:34 +0300

    Update Transport.java

--- src/com/GitTest/Transport.java
+++ src/com/GitTest/Transport.java
@@ -1,7 +1,10 @@
 package com.GitTest;
 
+import java.util.Scanner;
+
 public class Transport {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
         System.out.println("Hellow World!");
     }
 }
